7f89d8ec42fb9756c443712eef95400d265ee29a
estella.hickl-szabo <estella.hickl-szabo@KK-GYM-EDV1-12.gym.kk.local>
2018-11-27 17:41:57 +0100

    Kommentare hinzugefügt

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -3,7 +3,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hallo Klasse");
-	}
-
-}
+		System.out.println("Hallo Klasse");
+		//das ist ein Kommentar, kein Code der ausgeführt wird
+		//das ist ein einzeiliger Kommentar
+		/*
+}		*Mehrzeiligen KOmmentar schreiben
+		*/
