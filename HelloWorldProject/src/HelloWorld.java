98a62d69e0e3dc8e5dd10d86c12caad6507e3b2e
estella.hickl-szabo <estella.hickl-szabo@KK-GYM-EDV1-12.gym.kk.local>
2018-12-18 18:07:02 +0100

    hallo

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -1,16 +1,1 @@
 
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hallo Estella");
-		//das ist ein Kommentar, kein Code der ausgeführt wird
-		//das ist ein einzeiliger Kommentar
-		/*
-		*Mehrzeiligen Kommentar schreiben
-		*/
-		System.out.println("Hallo Klasse");
-		System.out.println("Schönen Abend");
-		
-	}
-}
