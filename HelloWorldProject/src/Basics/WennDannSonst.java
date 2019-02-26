2510c922526d1a12c161162b7219038931b09382
estella.hickl-szabo <estella.hickl-szabo@KK-GYM-EDV1-14.gym.kk.local>
2019-02-26 18:07:16 +0100

    wertzu

---
+++ HelloWorldProject/src/Basics/WennDannSonst.java
@@ -0,0 +1,30 @@
+package Basics;
+
+public class WennDannSonst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Wenn, dann ... →sonst ...
+
+				// Wenn -dann:
+				if (1 < 3) {
+					System.out.println("1 < 3");
+				}
+				//
+				boolean trifft_zu = true;
+				// boolean ist eine Variabel, die speichert, ob etwas "true or false" ist
+				// (→siehe Variabeln)
+				if (trifft_zu == true) {
+					System.out.println("Juhu!");
+				}
+				// wenn dann sonst:
+				int zahl = 2;
+				if (zahl == 1) {
+					System.out.println("ungerade");
+				} else {
+					System.out.println("gerade");
+				}
+
+			}
+	}
+
---
+++ HelloWorldProject/src/Sortieren/Bubblesort.java
@@ -0,0 +1,32 @@
+package sortieren;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// Bubblesort Array
+		// Länge der Reihe
+		int[] zahlen = { 2, 4, 3, 5, 7, 4, 9, 8 };
+		System.out.println("Die Reihe hat " + zahlen.length + " Zahlen");
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.println(zahlen[i]);
+		}
+		//
+		System.out.println("Bubblesort");
+		//
+		// Bubblesort
+		for (int z = zahlen.length; z > 1; z--) {
+			for (int s = 0; s < z - 1; s++) {
+				if (zahlen[s] > zahlen[s + 1]) {
+					int a = zahlen[s];
+					zahlen[s] = zahlen[s + 1];
+					zahlen[s + 1] = a;
+
+				}
+			}
+		}
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.println(zahlen[i]);
+		}
+
+	}
+}
