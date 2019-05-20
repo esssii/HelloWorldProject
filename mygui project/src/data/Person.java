dbdabe5ecfdc08101eddce48a49c8153581ac11f
estel <estel@192.168.1.110>
2019-05-20 19:42:51 +0200

    123

---
+++ mygui project/src/data/Person.java
@@ -0,0 +1,60 @@
+package data;
+
+import java.util.ArrayList;
+
+public class Person {
+	//
+	private static ArrayList<Person> liste = new ArrayList<Person>();
+	//
+	private String vorname;
+	private String nachname;
+	private String plz;
+	private String ort;
+	private String strasse;
+	private String hausnummer;
+	//
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+	public String getPlz() {
+		return plz;
+	}
+	public void setPlz(String plz) {
+		this.plz = plz;
+	}
+	public String getOrt() {
+		return ort;
+	}
+	public void setOrt(String ort) {
+		this.ort = ort;
+	}
+	public String getStrasse() {
+		return strasse;
+	}
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+	public String getHausnummer() {
+		return hausnummer;
+	}
+	public void setHausnummer(String hausnummer) {
+		this.hausnummer = hausnummer;
+	}
+	
+	@Override
+	public String toString() {
+		return getNachname() + ", " + getVorname();
+	}
+	public static ArrayList<Person> getListe() {
+		return liste;
+	}
+}
---
+++ mygui project/src/test/PersonTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import data.Person;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		//
+		String PersonTest ;
+		//
+		Person p1; // Variablen Definition
+		p1 = new Person(); // Instanzierung
+		//
+		p1.setVorname("Hans");
+		p1.setNachname("Huber");
+		p1.setStrasse("wienerstrasse");
+		p1.setHausnummer("123");
+		p1.setOrt("Wien");
+		p1.setPlz("1234");
+		
+		//
+		System.out.println(p1);
+		
+
+	}
+
+}
